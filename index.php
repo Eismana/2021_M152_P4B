961184b7734688a98b1f04617444b5468f0da849
Eismana <49439071+Eismana@users.noreply.github.com>
2021-01-28 15:04:37 +0100

    Add chapter 1

---
+++ index.php
@@ -0,0 +1,117 @@
+<!--
+   index.php
+   Nom du projet: facebook-cfpt
+   Auteur : Eisman Camara Abel
+   Crée le : 28.01.2021
+   Mis a jour le : 28.01.2021
+-->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <meta charset="utf-8">
+        <title>cfpt-facebook-Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <!--[if lt IE 9]>
+          <script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+        <link href="assets/css/facebook.css" rel="stylesheet">
+    </head>
+    
+    <body>
+        
+        <div class="wrapper">
+			<div class="box">
+				<div class="row row-offcanvas row-offcanvas-left">
+					
+					
+				  
+					<!-- main right col -->
+					<div class="column col-sm-12 col-xs-12" id="main">
+						
+						<!-- top nav -->
+						<div class="navbar navbar-blue navbar-static-top">  
+							<div class="navbar-header">
+							  <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+								<span class="sr-only">Toggle</span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+								<span class="icon-bar"></span>
+							  </button>
+							  <a href="index.php" class="navbar-brand logo">b</a>
+							</div>
+							<nav class="collapse navbar-collapse" role="navigation">
+							<form class="navbar-form navbar-left">
+								<div class="input-group input-group-sm" style="max-width:360px;">
+								  <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
+								  <div class="input-group-btn">
+									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+								  </div>
+								</div>
+							</form>
+							<ul class="nav navbar-nav">
+							  <li>
+								<a href="index.php"><i class="glyphicon glyphicon-home"></i> Home</a>
+							  </li>
+							  <li>
+								<a href="Post.php"><i class="glyphicon glyphicon-plus"></i> Post</a>
+                </li>
+              </ul>
+
+							</nav>
+						</div>
+						<!-- /top nav -->
+					  
+						<div class="padding">
+							<div class="full col-sm-9">
+							  
+								<!-- content -->                      
+								<div class="row">
+								  
+								 <!-- main col left --> 
+								 <div class="col-sm-5">
+                 
+									  <div class="panel panel-default" style="max-width:200px;">
+										<div class="panel-thumbnail"  ><img src="assets/img/bg_5.jpg" class="img-responsive" width="200x"></div>
+										<div class="panel-body ">
+										  <p class="lead">Jean</p>
+										  <p>
+											<img src="assets/img/uFp_tsTJboUY7kue5XAsGAs28.png" height="28px" width="28px">
+										  </p>
+										</div>
+									  </div>
+								  </div>
+								  
+								  <!-- main col right -->
+								  <div class="col-sm-7">
+                  <h1>Welcome</h1> 
+							   </div><!--/row-->
+							</div><!-- /col-9 -->
+						</div><!-- /padding -->
+					</div>
+					<!-- /main -->
+				  
+				</div>
+			</div>
+		</div>
+
+
+
+        
+        <script type="text/javascript" src="assets/js/jquery.js"></script>
+        <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+        <script type="text/javascript">
+        $(document).ready(function() {
+			$('[data-toggle=offcanvas]').click(function() {
+				$(this).toggleClass('visible-xs text-center');
+				$(this).find('i').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+				$('.row-offcanvas').toggleClass('active');
+				$('#lg-menu').toggleClass('hidden-xs').toggleClass('visible-xs');
+				$('#xs-menu').toggleClass('visible-xs').toggleClass('hidden-xs');
+				$('#btnShow').toggle();
+			});
+        });
+        </script>
+</body>
+</html>
