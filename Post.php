8227f4ce99da0d5a05c2f4ca86479e3fd8d0167b
Eismana <49439071+Eismana@users.noreply.github.com>
2021-01-28 15:37:07 +0100

    Update navbar Chapter 1

--- Post.php
+++ Post.php
@@ -42,15 +42,7 @@
 							  </button>
 							  <a href="#" class="navbar-brand logo">b</a>
 							</div>
-							<nav class="collapse navbar-collapse" role="navigation">
-							<form class="navbar-form navbar-left">
-								<div class="input-group input-group-sm" style="max-width:360px;">
-								  <input class="form-control" placeholder="Search" name="srch-term" id="srch-term" type="text">
-								  <div class="input-group-btn">
-									<button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
-								  </div>
-								</div>
-							</form>
+							<nav class="collapse navbar-collapse" role="navigation">		
 							<ul class="nav navbar-nav">
 							  <li>
 								<a href="index.php"><i class="glyphicon glyphicon-home"></i> Home</a>
